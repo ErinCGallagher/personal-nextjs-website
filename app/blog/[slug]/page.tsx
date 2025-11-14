f2d3cf82bbf22a36a0d5c0e84e2e711380dbaff6
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2025-11-14 14:42:34 +0000

    get mdx setup

---
+++ app/blog/[slug]/page.tsx
@@ -0,0 +1,25 @@
+
+import { notFound } from 'next/navigation'
+import { getBlogPosts } from '../utils'
+
+export default async function Page({ params }: { params: Promise<{ slug: string }> }) {
+  const { slug } = await params
+  let post = getBlogPosts().find((post) => post.slug === slug)
+
+  if (!post) {
+    notFound()
+  }
+
+  return (
+    <section>
+      <h1 className="title font-semibold text-2xl tracking-tighter">
+        {post.metadata.title}
+      </h1>
+      <div className="flex justify-between items-center mt-2 mb-8 text-sm">
+        <p className="text-sm text-neutral-600 dark:text-neutral-400">
+          {post.metadata.publishedAt}
+        </p>
+      </div>
+    </section>
+  )
+}
---
+++ app/blog/page.tsx
@@ -0,0 +1,7 @@
+export default function Page() {
+    return (
+        <h1 className="max-w-xs text-3xl font-semibold leading-10 tracking-tight text-black dark:text-zinc-50">
+           Blog Page
+        </h1>
+    )
+}
