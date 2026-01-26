9b9ccc041cc018b5dd7b8ac60cc9bd39b1e9f97b
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-01-26 15:23:27 -0600

    update bio

--- app/components/about.tsx
+++ app/components/about.tsx
@@ -18,7 +18,7 @@ export function About() {
         </div>
         <div className="flex-1">
           <p className="text-base text-gray-700 dark:text-gray-300 leading-relaxed text-center md:text-left">
-            I'm a Staff Engineer who specializes in leading cross-platform teams. Most recently, I spent a year as the domain expert at Uber, where I defined the technical vision for video and launched its first use case. When I'm not debugging code, you'll find me hiking trails and probably talking about my latest camping adventure.
+            I'm a Staff Engineer, most recently at Uber, where I was the domain expert for video and was a founding member of the Ads team. I defined the engineering vision and architecture for video at scale across Uber. I led the team building Uber's first major video product; a key innovation in a highly competitive market. As a technical leader, I specializes in architecting scalable systems and driving 0-to-1 initiatives. When I'm not debugging code, you'll find me hiking trails and probably talking about my latest camping adventure.
           </p>
           <div className="mt-4 text-center md:text-left">
             <Link
