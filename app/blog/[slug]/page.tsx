53e70338d9a977e5f6b8550d3aefeacafa9ef360
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-01-29 13:06:57 -0600

    fix spacing on blog posts

--- app/blog/[slug]/page.tsx
+++ app/blog/[slug]/page.tsx
@@ -47,7 +47,7 @@ export default async function Page({
         </div>
       )}
       <div className="px-4 sm:px-6 py-16">
-        <main className="max-w-4xl mx-auto p-6 md:p16 bg-white text-foreground rounded-lg">
+        <main className="max-w-4xl mx-auto px-8 md:px-16 py-12 bg-white text-foreground rounded-lg">
           <div className="flex items-center gap-3 mb-4">
             <div className="relative w-10 h-10 flex-shrink-0">
               <Image
