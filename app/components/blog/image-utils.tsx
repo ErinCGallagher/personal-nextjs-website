6be27b8eed2747241078893cb3d17a3b36fb8448
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-01-29 20:13:32 -0600

    fix compiler erorr

--- app/components/blog/image-utils.tsx
+++ app/components/blog/image-utils.tsx
@@ -1,12 +1,13 @@
-import { ReactNode, Children, cloneElement, isValidElement } from "react";
+import { ReactNode, Children, cloneElement, isValidElement, ReactElement } from "react";
 
 export function addRoundedCornersToImages(children: ReactNode): ReactNode[] {
   return Children.toArray(children).map((child) => {
     if (isValidElement(child)) {
-      return cloneElement(child, {
-        ...child.props,
-        className: `rounded-lg ${child.props.className || ""}`,
-      } as any);
+      const props = child.props as Record<string, unknown>;
+      return cloneElement(child as ReactElement, {
+        ...props,
+        className: `rounded-lg ${props.className || ""}`,
+      });
     }
     return child;
   });
