af3618a5d979a73882e9e61354c4865c402724f7
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-02-18 19:35:40 -0500

    fix rate limiter bug with ipKeyGenerator

--- backend/src/rate-limiters.ts
+++ backend/src/rate-limiters.ts
@@ -1,9 +1,12 @@
 import rateLimit, { ipKeyGenerator } from 'express-rate-limit';
 
+const isTest = process.env.NODE_ENV === "test";
+
 export const globalLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 min
-  max: 60, // 100 requests in 1 min
-  keyGenerator: (req) => ipKeyGenerator(req) || "unknown",
+  max: 60, // 60 requests in 1 min
+  keyGenerator: (req, res) => ipKeyGenerator(req, res) || "unknown",
+  skip: () => isTest,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -11,7 +14,8 @@ export const globalLimiter = rateLimit({
 export const ipLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 min
   max: 5, // 5 requests in 1 min
-  keyGenerator: (req) => ipKeyGenerator(req) || "unknown",
+  keyGenerator: (req, res) => ipKeyGenerator(req, res) || "unknown",
+  skip: () => isTest,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -19,7 +23,8 @@ export const ipLimiter = rateLimit({
 export const anonymousIdLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 min
   max: 5, // 5 requests in 1 min
-  keyGenerator: (req) => req.body.anonymous_id || ipKeyGenerator(req),
+  keyGenerator: (req, res) => req.body.anonymous_id || ipKeyGenerator(req, res) || "unknown",
+  skip: () => isTest,
   standardHeaders: true,
   legacyHeaders: false,
 });
@@ -27,7 +32,8 @@ export const anonymousIdLimiter = rateLimit({
 export const readLimiter = rateLimit({
   windowMs: 60 * 1000, // 1 min
   max: 20, // 20 requests in 1 min
-  keyGenerator: (req) => ipKeyGenerator(req) || "unknown",
+  keyGenerator: (req, res) => ipKeyGenerator(req, res) || "unknown",
+  skip: () => isTest,
   standardHeaders: true,
   legacyHeaders: false,
 });
