3dee511ba7d2ab6058adac73157c3adfa8c3c7a8
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-02-18 21:05:43 -0500

    add ssl config to pool

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -7,6 +7,10 @@ const connectionString =
     ? process.env.TEST_DATABASE_URL
     : process.env.DATABASE_URL;
 
-const pool = new Pool({ connectionString });
+
+const pool = new Pool({
+  connectionString,
+  ssl: process.env.NODE_ENV === "production" ? { rejectUnauthorized: false } : false
+});
 
 export default pool;
