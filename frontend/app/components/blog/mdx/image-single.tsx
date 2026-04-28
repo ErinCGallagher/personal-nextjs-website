66d2dc1eb9979b1aec68e4e03a8e56896d6f620c
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-04-28 18:58:19 +0300

    add image sizes

--- frontend/app/components/blog/mdx/image-single.tsx
+++ frontend/app/components/blog/mdx/image-single.tsx
@@ -3,6 +3,7 @@ import { addRoundedCornersToImages } from "./image-utils";
 
 interface ImageSingleProps {
   children: ReactNode;
+  description1?: string;
 }
 
 /**
@@ -21,12 +22,15 @@ export function ImageSingleVertical({ children }: ImageSingleProps) {
 /**
  * ImageSingleHorizontal - Displays a single landscape image centered with wider constrained width
  */
-export function ImageSingleHorizontal({ children }: ImageSingleProps) {
+export function ImageSingleHorizontal({ children, description1 }: ImageSingleProps) {
   const images = addRoundedCornersToImages(children);
 
   return (
     <div className="flex justify-center my-6">
-      <div className="max-w-2xl">{images[0]}</div>
+      <div className="max-w-2xl">
+        {images[0]}
+        {description1 && <p className="image-description text-center italic !mt-2">{description1}</p>}
+      </div>
     </div>
   );
 }
