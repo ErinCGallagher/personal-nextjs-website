67afe5100ff425235c95f9285a3288ebe57492df
Erin Gallagher <erin.catherine.gallagher@gmail.com>
2026-04-30 13:29:22 +0300

    update package files

--- frontend/app/layout.tsx
+++ frontend/app/layout.tsx
@@ -3,7 +3,7 @@ import { Geist, Geist_Mono, Playfair_Display } from "next/font/google";
 import "./globals.css";
 import Header from "./components/header";
 import Footer from "./components/footer";
-import { Analytics } from "@vercel/analytics/react";
+import { Analytics } from "@vercel/analytics/next";
 import Script from "next/script";
 
 const geistSans = Geist({
